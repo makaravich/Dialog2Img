e6000c486f843bb5cce67dd1ec1d8655e399e208
makaravich <d.makarski@gmail.com>
2025-03-22 12:35:17 +0100

    Add Composer support

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'makaravich/dialog2img',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6a8fad35cdb86f72c595224b1fecffc36acca530',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'makaravich/dialog2img' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6a8fad35cdb86f72c595224b1fecffc36acca530',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
